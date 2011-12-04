cb8e3441861018d9703123657b1141c1ec7d16a2
Rob McVey <robmcvey@gmail.com>
2011-12-04 20:34:13 +0000

    tabs, you so silly

--- Controller/PaymentsController.php
+++ Controller/PaymentsController.php
@@ -7,11 +7,11 @@ App::uses('AppController', 'Controller');
 
 class PaymentsController extends AppController {
 
-  // Include the Payapl component
+	// Include the Payapl component
 	public $components = array('Paypal');
   
-  // Set the values and begin paypal process
-  public function express_checkout() {
+  	// Set the values and begin paypal process
+  	public function express_checkout() {
 		$this->Paypal->amount = 10.00;
 		$this->Paypal->currencyCode = 'GBP';	
 		$this->Paypal->returnUrl = Router::url(array('action' => 'get_details'), true);
@@ -20,24 +20,24 @@ class PaymentsController extends AppController {
 		$this->Paypal->itemName = 'Swedish penis enlargement kit';
 		$this->Paypal->quantity = 1;
 		$this->Paypal->expressCheckout();
-  }
+  	}
   
-  // Use the token in the return URL to fetch details
-  public function get_details() {
-    $this->Paypal->token = $this->request->query['token'];
-    $this->Paypal->payerId = $this->request->query['PayerID'];
+	// Use the token in the return URL to fetch details
+  	public function get_details() {
+    		$this->Paypal->token = $this->request->query['token'];
+    		$this->Paypal->payerId = $this->request->query['PayerID'];
 		$customer_details = $this->Paypal->getExpressCheckoutDetails();
-    debug($customer_details);
-  }
+    		debug($customer_details);
+  	}
   
-  // Complete the payment, pass back the token and payerId
-  public function complete_express_checkout($token,$payerId) {
-    $this->Paypal->amount = 10.00;
+  	// Complete the payment, pass back the token and payerId
+  	public function complete_express_checkout($token,$payerId) {
+    		$this->Paypal->amount = 10.00;
 		$this->Paypal->currencyCode = 'GBP';
-    $this->Paypal->token = $token;
+    		$this->Paypal->token = $token;
 		$this->Paypal->payerId = $payerId;
 		$response = $this->Paypal->doExpressCheckoutPayment(); 
-    debug($response);
-  }
+    		debug($response);
+  	}
 
 }
