63e36ce08c25f9689ebb22fdf01539016e1dfd4f
Rob McVey <robmcvey@gmail.com>
2011-12-04 20:22:51 +0000

    Added the new method and variables for Direct Payments

--- Controller/Component/Paypal.php
+++ Controller/Component/Paypal.php
@@ -11,7 +11,7 @@ App::uses('HttpSocket', 'Network/Http');
 class PaypalComponent extends Component {
 
 	// Live v Sandbox mode !important
-	public $sandbox_mode = true;
+	public $sandboxMode = true;
 
 	// Live paypal API config
 	public $config = array(
@@ -23,34 +23,34 @@ class PaypalComponent extends Component {
 	);
 
 	// Sandbox paypal API config
-	public $sandbox_config = array(
+	public $sandboxConfig = array(
 		'webscr' => 'https://www.sandbox.paypal.com/webscr/',
 		'endpoint' => 'https://api.sandbox.paypal.com/nvp/',
-		'password' => '12345678',
-		'email' => 'robmcv_125578377blahblah.gmail.com',
-		'signature' => 'A43525523523452354SQTV.0UUxbV4NuL2'
+		'password' => '13222',
+		'email' => 'robmcv_12iz_api1.gmail.com',
+		'signature' => 'An5ns1KsViSQTV.0UUxbV4NuL2'
 	);
 
 	// API version
-	public $api_version = '53.0';
+	public $apiVersion = '53.0';
 	
 	// Return URL for express payments
-	public $return_url = '';
+	public $returnUrl = '';
 	
 	// Cancel URL for Express payments cancelled
-	public $cancel_url = '';
+	public $cancelUrl = '';
 
 	// Default Currency code
-	public $currency_code = 'GBP';
+	public $currencyCode = 'GBP';
 
 	//The amount of the transaction For example, EUR 2.000,00 must be specified as 2000.00 or 2,000.00.	
-	public $amount = 0;
+	public $amount = null;
 	
 	// Customise Express checkout with a description (api version > 53)
-	public $item_name = '';
+	public $itemName = '';
 	
 	// Customise Express checkout with a description (api version > 53)
-	public $order_desc = '';
+	public $orderDesc = '';
 	
 	// optional quantity
 	public $quantity = 1;
@@ -59,33 +59,59 @@ class PaypalComponent extends Component {
 	public $token = null;
 	
 	// The payers paypal ID 
-	public $payer_id = null;
+	public $payerId = null;
+	
+	// Credit card details
+  	public $creditCardNumber = '';
+	public $creditCardType = '';
+	public $creditCardExpires = '';
+	public $creditCardCvv = '';
+	
+	// Customer details
+	public $customerSalutation = '';
+	public $customerFirstName = '';
+	public $customerMiddleName = '';
+	public $customerLastName = '';
+	public $customerSuffix = '';
+	
+	// Billing details
+	public $billingAddress1 = '';
+	public $billingAddress2 = '';
+	public $billingCity = '';
+	public $billingState = '';
+	public $billingCountryCode = '';
+	public $billingZip = '';
+	
+	// Users IP address
+	public $ipAddress = '';
 	
 	// controller reference
-	protected $controller = null;
+	protected $_controller = null;
 	
 	
 	/**
-	 * undocumented function
+	 * Start up, gets an instance on the controller class (needed for redirect) sets 
+	 * the config (live or sandbox) and sets the users IP 
 	 *
 	 * @return void
 	 * @author Rob Mcvey
 	 **/
 	public function initialize($controller) {
-		$this->controller = $controller;		
-		if($this->sandbox_mode) {
-			$this->config = $this->sandbox_config;
+		$this->_controller = $controller;		
+		$this->ipAddress = $_SERVER['REMOTE_ADDR'];
+		if($this->sandboxMode) {
+			$this->config = $this->sandboxConfig;
 		}
 	}
 	
 	
 	/**
-	 * undocumented function
+	 * Generated a fresh token and redirects the use to the paypal page
 	 *
 	 * @return void
 	 * @author Rob Mcvey
 	 **/
-	public function ExpressCheckout() {
+	public function expressCheckout() {
 		
 		// We dont have a valid amount	
 		if(!isset($this->amount) || empty($this->amount) || !is_numeric($this->amount)) {
@@ -93,12 +119,13 @@ class PaypalComponent extends Component {
 		}
 		
 		// Call the SetExpressCheckout method to get a fresh token
-		$token = $this->SetExpressCheckout();
+		$token = $this->setExpressCheckout();
 		
 		// We have a token, redirect to paypals web server (not the URL is different to the API endpoint)
 		if($token) {
-			$this->controller->redirect($this->config['webscr'].'?cmd=_express-checkout&token='.$token);
+			$this->_controller->redirect($this->config['webscr'].'?cmd=_express-checkout&token='.$token);
 		} else {
+			$this->log($token , 'paypal');
 			throw new Exception(__('The was a problem with the payment gateway'));
 		}
 	}
@@ -109,51 +136,53 @@ class PaypalComponent extends Component {
 	 * operation to provide sufficient information to initiate the payment flow and redirect 
 	 * to PayPal if the operation was successful with the token sent back from Paypal
 	 *
-	 * @return void
+	 * @return string $token A token to be used when redirecting the user to PayPal
 	 * @author Rob Mcvey
 	 **/
-	public function SetExpressCheckout() {
+	public function setExpressCheckout() {
 			
-		$set_express_checkout_nvp = array(
+		// Build the NVPs (Named value pairs)	
+		$setExpressCheckoutNvp = array(
 			'METHOD' => 'SetExpressCheckout',
-			'VERSION' => $this->api_version,
+			'VERSION' => $this->apiVersion,
 			'USER' => $this->config['email'],
 			'PWD' => $this->config['password'],									
 			'SIGNATURE' => $this->config['signature'],
-			'CURRENCYCODE' => $this->currency_code,
-			'RETURNURL' => $this->return_url,
-			'CANCELURL' => $this->cancel_url,
+			'CURRENCYCODE' => $this->currencyCode,
+			'RETURNURL' => $this->returnUrl,
+			'CANCELURL' => $this->cancelUrl,
 			'PAYMENTACTION' => 'ORDER',
 			'PAGESTYLE' => 'Copify',
 			'AMT' => $this->amount,
-			'L_NAME0'=> $this->item_name,    
-			'L_DESC0'=> $this->order_desc, 
+			'L_NAME0'=> $this->itemName,    
+			'L_DESC0'=> $this->orderDesc, 
 			'L_AMT0'=> $this->amount, 
 			'L_QTY0' => $this->quantity,													
 		);	
 
 		// HTTPSocket class		
-		$HttpSocket = new HttpSocket();	
+		$httpSocket = new HttpSocket();	
 
 		// Post the NVPs to the relevent endpoint
-		$response = $HttpSocket->post($this->config['endpoint'] , $set_express_checkout_nvp);
+		$response = $httpSocket->post($this->config['endpoint'] , $setExpressCheckoutNvp);
 		
 		// Parse the guff that comes back from paypal
 		parse_str($response->body , $parsed);
-		
-		//debug($parsed);
 		
 		// Return the token, or throw a human readable error
 		if(array_key_exists('TOKEN', $parsed) && array_key_exists('ACK', $parsed) && $parsed['ACK'] == 'Success') {
 			return $parsed['TOKEN'];
 		}
 		elseif(array_key_exists('ACK', $parsed) && array_key_exists('L_LONGMESSAGE0', $parsed) && $parsed['ACK'] != 'Success') {
+			$this->log($parsed , 'paypal');	
 			throw new Exception($parsed['ACK'] . ' : ' . $parsed['L_LONGMESSAGE0']);
 		}
 		elseif(array_key_exists('ACK', $parsed) && array_key_exists('L_ERRORCODE0', $parsed) && $parsed['ACK'] != 'Success') {
+			$this->log($parsed , 'paypal');	
 			throw new Exception($parsed['ACK'] . ' : ' . $parsed['L_ERRORCODE0']);
 		} 
 		else {
+			$this->log($parsed , 'paypal');	
 			throw new Exception(__('There is a problem with the payment gateway. Please try again later.'));
 		}
 	}
@@ -162,24 +191,27 @@ class PaypalComponent extends Component {
 	/**
 	 * To obtain details about an Express Checkout transaction, you can invoke the 
 	 * GetExpressCheckoutDetails API operation. 
-	 * @return void
+	 * 
+	 * @return array $parsed An array of fields with the customers details, or throws and exception
 	 * @author Rob Mcvey
 	 **/
-	public function GetExpressCheckoutDetails() {
-		$get_express_checkout_details_nvp = array(
+	public function getExpressCheckoutDetails() {
+			
+		// Build the NVPs (Named value pairs)	
+		$getExpressCheckoutDetailsNvp = array(
 			'METHOD' => 'GetExpressCheckoutDetails' , 
 			'TOKEN' => $this->token,
-			'VERSION' => $this->api_version,
+			'VERSION' => $this->apiVersion,
 			'USER' => $this->config['email'],
 			'PWD' => $this->config['password'],									
 			'SIGNATURE' => $this->config['signature'],
 		);
 		
 		// HTTPSocket class		
-		$HttpSocket = new HttpSocket();	
+		$httpSocket = new HttpSocket();	
 
 		// Post the NVPs to the relevent endpoint
-		$response = $HttpSocket->post($this->config['endpoint'] , $get_express_checkout_details_nvp);
+		$response = $httpSocket->post($this->config['endpoint'] , $getExpressCheckoutDetailsNvp);
 
 		// Parse the guff that comes back from paypal
 		parse_str($response , $parsed);
@@ -189,12 +221,15 @@ class PaypalComponent extends Component {
 			return $parsed;
 		}
 		elseif(array_key_exists('ACK', $parsed) && array_key_exists('L_LONGMESSAGE0', $parsed) && $parsed['ACK'] != 'Success') {
+			$this->log($parsed , 'paypal');	
 			throw new Exception($parsed['ACK'] . ' : ' . $parsed['L_LONGMESSAGE0']);
 		}
 		elseif(array_key_exists('ACK', $parsed) && array_key_exists('L_ERRORCODE0', $parsed) && $parsed['ACK'] != 'Success') {
+			$this->log($parsed , 'paypal');	
 			throw new Exception($parsed['ACK'] . ' : ' . $parsed['L_ERRORCODE0']);
 		} 
 		else {
+			$this->log($parsed , 'paypal');
 			throw new Exception(__('There is a problem with the payment gateway. Please try again later.'));
 		}	
 	}
@@ -204,28 +239,30 @@ class PaypalComponent extends Component {
 	 * To complete an Express Checkout transaction, you must invoke the 
 	 * DoExpressCheckoutPayment API operation. 
 	 *
-	 * @return void
+	 * @return array $parsed An array of fields with the payment info, or throws and exception
 	 * @author Rob Mcvey
 	 **/
-	public function DoExpressCheckoutPayment() {
-		$do_express_checkout_payment = array(
+	public function doExpressCheckoutPayment() {
+		
+		// Build the NVPs (Named value pairs)	
+		$doExpressCheckoutPaymentNvp = array(
 			'METHOD' => 'DoExpressCheckoutPayment' ,
 			'USER' => $this->config['email'],
 			'PWD' => $this->config['password'],									
 			'SIGNATURE' => $this->config['signature'],
-			'VERSION' => $this->api_version,
+			'VERSION' => $this->apiVersion,
 			'TOKEN' => $this->token,
-			'PAYERID' => $this->payer_id,	
+			'PAYERID' => $this->payerId,	
 			'PAYMENTACTION' => 'Sale',
-			'CURRENCYCODE' => $this->currency_code,
+			'CURRENCYCODE' => $this->currencyCode,
 			'AMT'=> $this->amount													
 		);
 		
 		// HTTPSocket class		
-		$HttpSocket = new HttpSocket();	
+		$httpSocket = new HttpSocket();	
 
 		// Post the NVPs to the relevent endpoint
-		$response = $HttpSocket->post($this->config['endpoint'] , $do_express_checkout_payment);
+		$response = $httpSocket->post($this->config['endpoint'] , $doExpressCheckoutPaymentNvp);
 
 		// Parse the guff that comes back from paypal
 		parse_str($response , $parsed);
@@ -235,15 +272,90 @@ class PaypalComponent extends Component {
 			return $parsed;
 		}
 		elseif(array_key_exists('ACK', $parsed) && array_key_exists('L_LONGMESSAGE0', $parsed) && $parsed['ACK'] != 'Success') {
+			$this->log($parsed , 'paypal');	
 			throw new Exception($parsed['ACK'] . ' : ' . $parsed['L_LONGMESSAGE0']);
 		}
 		elseif(array_key_exists('ACK', $parsed) && array_key_exists('L_ERRORCODE0', $parsed) && $parsed['ACK'] != 'Success') {
+			$this->log($parsed , 'paypal');	
 			throw new Exception($parsed['ACK'] . ' : ' . $parsed['L_ERRORCODE0']);
 		} 
 		else {
+			$this->log($parsed , 'paypal');
 			throw new Exception(__('There is a problem with the payment gateway. Please try again later.'));
 		}		
 	}
-	
-	
+
+
+	/**
+	 * The DoDirectPayment API Operation enables you to process a credit card payment.
+	 *
+	 * @return array $parsed An array of fields with the payment info, or throws and exception
+	 * @author Rob Mcvey
+	 **/
+	public function doDirectPayment() {
+			
+		// Build the NVPs (Named value pairs)	
+		$doDirectPaymentNvp = array(
+		    'METHOD' => 'DoDirectPayment',
+		    'PAYMENTACTION' => 'SALE',
+		    'VERSION' => $this->apiVersion,
+		    'AMT' => $this->amount,
+		    'CURRENCYCODE' => $this->currencyCode,
+		    'IPADDRESS' => $this->ipAddress,
+		    'USER' => $this->config['email'],
+			'PWD' => $this->config['password'],									
+			'SIGNATURE' => $this->config['signature'],
+			
+		    // Credit Card Details
+		    'CREDITCARDTYPE' => $this->creditCardType,
+		    'ACCT' => $this->creditCardNumber,
+		    'EXPDATE' => $this->creditCardExpires,
+		    'CVV2' => $this->creditCardCvv,
+		    
+		    // Customer Details
+		    'SALUTATION' => $this->customerSalutation,
+		    'FIRSTNAME' => $this->customerFirstName,
+		    'MIDDLENAME' => $this->customerMiddleName,
+		    'LASTNAME' => $this->customerLastName,
+		    'SUFFIX' => $this->customerSuffix,
+		    
+		    // Billing Address
+		    'STREET' => $this->billingAddress1,
+		    'STREET2' => $this->billingAddress2,
+		    'CITY' => $this->billingCity,
+		    'STATE' => $this->billingState,
+		    'COUNTRYCODE' => $this->billingCountryCode,
+		    'ZIP' => $this->billingZip,
+		);	
+		
+		$this->log($doDirectPaymentNvp);
+		
+		// HTTPSocket class		
+		$httpSocket = new HttpSocket();	
+
+		// Post the NVPs to the relevent endpoint
+		$response = $httpSocket->post($this->config['endpoint'] , $doDirectPaymentNvp);
+
+		// Parse the guff that comes back from paypal
+		parse_str($response , $parsed);
+		
+		// Return the token, or throw a human readable error
+		if(array_key_exists('ACK', $parsed) && $parsed['ACK'] == 'Success') {
+			return $parsed;
+		}
+		elseif(array_key_exists('ACK', $parsed) && array_key_exists('L_LONGMESSAGE0', $parsed) && $parsed['ACK'] != 'Success') {
+			$this->log($parsed , 'paypal');	
+			throw new Exception($parsed['ACK'] . ' : ' . $parsed['L_LONGMESSAGE0']);
+		}
+		elseif(array_key_exists('ACK', $parsed) && array_key_exists('L_ERRORCODE0', $parsed) && $parsed['ACK'] != 'Success') {
+			$this->log($parsed , 'paypal');	
+			throw new Exception($parsed['ACK'] . ' : ' . $parsed['L_ERRORCODE0']);
+		} 
+		else {
+			$this->log($parsed , 'paypal');
+			throw new Exception(__('There is a problem with the payment gateway. Please try again later.'));
+		}
+				
+	}
+		
 }
