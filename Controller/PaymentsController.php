25edf186ed8adbea5298f695f0c41164d1a43e80
Rob McVey <robmcvey@gmail.com>
2011-12-04 21:04:27 +0000

    catch exceptions in example

--- Controller/PaymentsController.php
+++ Controller/PaymentsController.php
@@ -12,44 +12,60 @@ class PaymentsController extends AppController {
   
   	// Set the values and begin paypal process
   	public function express_checkout() {
-		$this->Paypal->amount = 10.00;
-		$this->Paypal->currencyCode = 'GBP';	
-		$this->Paypal->returnUrl = Router::url(array('action' => 'get_details'), true);
-		$this->Paypal->cancelUrl = Router::url($this->here, true);
-		$this->Paypal->orderDesc = 'A description of the thing someone is about to buy';
-		$this->Paypal->itemName = 'Swedish penis enlargement kit';
-		$this->Paypal->quantity = 1;
-		$this->Paypal->expressCheckout();
+		try{
+			$this->Paypal->amount = 10.00;
+			$this->Paypal->currencyCode = 'GBP';	
+			$this->Paypal->returnUrl = Router::url(array('action' => 'get_details'), true);
+			$this->Paypal->cancelUrl = Router::url($this->here, true);
+			$this->Paypal->orderDesc = 'A description of the thing someone is about to buy';
+			$this->Paypal->itemName = 'Swedish penis enlargement kit';
+			$this->Paypal->quantity = 1;
+			$this->Paypal->expressCheckout();
+		} catch(Exception $e) {
+			$this->Session->setFlash($e->getMessage());
+		}
   	}
   
 	// Use the token in the return URL to fetch details
   	public function get_details() {
-    		$this->Paypal->token = $this->request->query['token'];
-    		$this->Paypal->payerId = $this->request->query['PayerID'];
-		$customer_details = $this->Paypal->getExpressCheckoutDetails();
-    		debug($customer_details);
+    		try {
+	    		$this->Paypal->token = $this->request->query['token'];
+	    		$this->Paypal->payerId = $this->request->query['PayerID'];
+			$customer_details = $this->Paypal->getExpressCheckoutDetails();
+	    		debug($customer_details);
+    		} catch(Exception $e) {
+			$this->Session->setFlash($e->getMessage());
+		}
   	}
   
   	// Complete the payment, pass back the token and payerId
   	public function complete_express_checkout($token,$payerId) {
-    		$this->Paypal->amount = 10.00;
-		$this->Paypal->currencyCode = 'GBP';
-    		$this->Paypal->token = $token;
-		$this->Paypal->payerId = $payerId;
-		$response = $this->Paypal->doExpressCheckoutPayment(); 
-    		debug($response);
+    		try{
+	    		$this->Paypal->amount = 10.00;
+			$this->Paypal->currencyCode = 'GBP';
+	    		$this->Paypal->token = $token;
+			$this->Paypal->payerId = $payerId;
+			$response = $this->Paypal->doExpressCheckoutPayment(); 
+	    		debug($response);
+    		} catch(Exception $e) {
+			$this->Session->setFlash($e->getMessage());
+		}
   	}
   	
   	// Do a direct credit card payment
   	public function charge_card() {
-  		$this->Paypal->amount = 10.00;
-		$this->Paypal->currencyCode = 'GBP';	
-		$this->Paypal->creditCardNumber = 'xxxxxxxxxxxx1234';
-		$this->Paypal->creditCardCvv = '123';
-		$this->Paypal->creditCardExpires = '012020';
-		$this->Paypal->creditCardType = 'Visa';
-		$result = $this->Paypal->doDirectPayment();
-		debug($result);
+  		try {
+	  		$this->Paypal->amount = 10.00;
+			$this->Paypal->currencyCode = 'GBP';	
+			$this->Paypal->creditCardNumber = 'xxxxxxxxxxxx1234';
+			$this->Paypal->creditCardCvv = '123';
+			$this->Paypal->creditCardExpires = '012020';
+			$this->Paypal->creditCardType = 'Visa';
+			$result = $this->Paypal->doDirectPayment();
+			debug($result);
+  		} catch(Exception $e) {
+			$this->Session->setFlash($e->getMessage());
+		}
   	}
 
 }
